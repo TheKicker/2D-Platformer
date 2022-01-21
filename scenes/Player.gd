f922707750a04f08beb6feedb6c683c84090a8c5
The Kicker <cav.lemasters@geneva.edu>
2022-01-21 13:02:14 -0500

    Tweaked mechanics, camera

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -7,6 +7,7 @@ var horizontalAcceleration = 2000
 var maxHorizontalSpeed = 140
 var jumpSpeed = 375
 var jumpTerminationMultiplier = 4
+var hasDoubleJump = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,9 +30,12 @@ func _process(delta):
 	# Limits our positive and negative speed to our maximmum horizontal speed
 	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 	
-	# Removes the ability to spam jump, player must be on the floor
-	if (moveVector.y < 0 && is_on_floor()):
+	# Removes the ability to spam jump, player must be on the floor or just left it (Coyote timer)
+	if (moveVector.y < 0 && (is_on_floor() || !$CoyoteTimer.is_stopped() || hasDoubleJump)):
 		velocity.y = moveVector.y * jumpSpeed
+		if(!is_on_floor() && $CoyoteTimer.is_stopped()):
+			hasDoubleJump = false
+		$CoyoteTimer.stop()
 	
 	# Allows the player to control the height of their jumps (tap versus hold)
 	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
@@ -39,8 +43,18 @@ func _process(delta):
 	else:
 		velocity.y += gravity * delta
 	
+	var wasOnFloor = is_on_floor()
+	
 	# Calculates the collisions and movements before looping to the next FPS
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if(wasOnFloor && !is_on_floor()):
+		$CoyoteTimer.start()
+	
+	if(is_on_floor()):
+		hasDoubleJump = true
+	
+	update_animation()
 
 func get_movement_vector():
 	# Vector2 is used for 2D Math calculations, if left 0 it amounts to False
@@ -52,3 +66,21 @@ func get_movement_vector():
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	
 	return moveVector
+
+func update_animation():
+	var moveVect = get_movement_vector()
+	
+	if(!is_on_floor()):
+		$AnimatedSprite.play("Jump")
+	elif(moveVect.x != 0):
+		$AnimatedSprite.play("Run")
+	else:
+		$AnimatedSprite.play("Idle")
+	
+	if(moveVect.x != 0):
+		$AnimatedSprite.flip_h = true if moveVect.x > 0 else false
+	
+	
+	
+	
+	
