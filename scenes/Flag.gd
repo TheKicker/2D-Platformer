629ffebddf22491af96a64f60444a7ac1cc0575b
The Kicker <cav.lemasters@geneva.edu>
2022-02-18 12:16:28 -0500

    New Level

--- scenes/Flag.gd
+++ scenes/Flag.gd
@@ -14,3 +14,13 @@ func _ready():
 
 func _on_Area2D_area_entered(area):
 	emit_signal("victory_royale")
+	vr_change_scene()
+	# get_tree().change_scene("res://scenes/Levels/Lvl_00"+str(int(get_tree().current_scene.filename[-8])+1)+".tscn")
+
+func vr_change_scene():
+	# get_tree().change_scene("res://scenes/Levels/Lvl_00"+str(int(get_tree().current_scene.filename[-8])+1)+".tscn")
+	var currentLevel = get_tree().current_scene.filename
+	print(currentLevel[-6])
+	currentLevel = int(currentLevel[-6])
+	var nextLevel = str(currentLevel + 1)
+	get_tree().change_scene("res://scenes/Levels/Lvl_00"+nextLevel+".tscn")
--- scenes/Friend.gd
+++ scenes/Friend.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+signal hello_there
+
 func _ready():
 	pass
 
 func _on_Area2D_area_entered(area):
 	$AnimatedSprite.play("show")
+	emit_signal("hello_there")
 
 
 func _on_Area2D_area_exited(area):
